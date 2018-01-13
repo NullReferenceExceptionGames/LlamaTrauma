c409882bfd3653b3552dfd9e8a34f065e535b689
brendon-ai <brendon-m@outlook.com>
2018-01-12 20:06:42 -0500

    Fixed disappearing enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -17,15 +17,14 @@ public class Enemy : MonoBehaviour
         var pathPointTransforms = parent.GetComponentsInChildren<Transform>();
         var points = new List<Vector3>();
         var timings = new List<float>();
-        var lastPosition = pathPointTransforms[0].position;
-        for (var i = 1; i <= pathPointTransforms.Length; i++)
+        for (var i = 0; i < pathPointTransforms.Length - 1; i++)
         {
-            var iLoop = i % pathPointTransforms.Length;
-            var currentTransformPosition = pathPointTransforms[iLoop].position;
-            points.Add(currentTransformPosition);
-            var timing = (lastPosition - currentTransformPosition).magnitude / unitsPerSecond;
+            var currentPosition = pathPointTransforms[i].position;
+            points.Add(currentPosition);
+            var timing = (pathPointTransforms[i + 1].position - currentPosition).magnitude / unitsPerSecond;
             timings.Add(timing);
         }
+        points.Add(pathPointTransforms[pathPointTransforms.Length - 1].position);
         pathPoints = points.ToArray();
         pathPointTimings = timings.ToArray();
         StartCoroutine(FollowPath());
@@ -34,11 +33,11 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     IEnumerator FollowPath()
     {
-        for (var i = 0; ; i = (i + 1) % pathPoints.Length)
+        for (var i = 0; i < pathPoints.Length - 1; i++)
         {
             lastIterStart = Time.timeSinceLevelLoad;
             var endTime = lastIterStart + pathPointTimings[i];
-            var nextI = (i + 1) % pathPoints.Length;
+            var nextI = (i + 1);
             var delta = pathPoints[nextI] - pathPoints[i];
             var timeDelta = endTime - lastIterStart;
             while (Time.timeSinceLevelLoad < endTime)
@@ -47,6 +46,11 @@ public class Enemy : MonoBehaviour
                 var newPosition = pathPoints[i] + (delta * interpolationCoefficient);
                 transform.position = newPosition;
                 yield return new WaitForFixedUpdate();
+            }
+            if (nextI == pathPoints.Length - 1)
+            {
+                Destroy(gameObject);
+                yield break;
             }
         }
     }
