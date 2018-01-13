41bcc4ce6f44e915e3ef3776df8b18653a99835b
brendon-ai <brendon-m@outlook.com>
2018-01-13 05:57:11 -0500

    Refactoring; prepared player shoot code

---
+++ Assets/Scripts/Projectile/PlayerShoot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    FiringState state = FiringState.NORMAL;
+    [SerializeField] GameObject normalBullet;
+    [SerializeField] GameObject bomb;
+
+    public void Shoot()
+    {
+
+        var ray = Camera.main.transform.rotation * Vector3.forward;
+        switch (state)
+        {
+            case FiringState.NORMAL:
+                var bullet = Instantiate(normalBullet);
+                bullet.GetComponent<Bullet>().SetIsTower(false);
+                NormalFire(bullet, ray * 100f);
+                break;
+            case FiringState.BOMB:
+                var bombInstance = Instantiate(bomb);
+                NormalFire(bomb, ray * 20f);
+                break;
+        }
+    }
+
+    void NormalFire(GameObject proj, Vector3 force)
+    {
+        proj.transform.position = Camera.main.transform.position;
+        proj.transform.rotation = Quaternion.LookRotation(force);
+        proj.GetComponent<Rigidbody>().AddForce(force);
+    }
+
+    enum FiringState
+    {
+        NORMAL,
+        BOMB
+    }
+}
