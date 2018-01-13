0ee7c793d5276ee9b0d16ccc6332c9448a555e6f
brendon-ai <brendon-m@outlook.com>
2018-01-13 04:10:25 -0500

    Shotgun

---
+++ Assets/Scripts/Tower/ShotgunTower.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotgunTower : Tower
+{
+    const float bulletSpeed = 30f;
+    const int numBullets = 5;
+    [SerializeField] GameObject hunter;
+    [SerializeField] GameObject bullet;
+
+    public override void Shoot(Transform enemy)
+    {
+        var delta = enemy.position - transform.position;
+        var force = delta.normalized * bulletSpeed;
+        var lookAngle = Quaternion.LookRotation(delta);
+        for (var i = 0; i < numBullets; i++)
+        {
+            var bul = Instantiate(bullet);
+            bul.transform.position = transform.position;
+            bul.transform.rotation = lookAngle;
+            bul.GetComponent<Bullet>().SetIsTower(true);
+            var randomAddition = new Vector3(randomVelocityAddition(), randomVelocityAddition(), randomVelocityAddition());
+            var forceRandom = force + randomAddition;
+            bul.GetComponent<Rigidbody>().AddForce(forceRandom);
+        }
+        hunter.transform.rotation = lookAngle;
+    }
+
+    float randomVelocityAddition()
+    {
+        return Random.Range(-5f, 5f);
+    }
+
+    public override float GetFiringDelay()
+    {
+        return 0.4f;
+    }
+
+    public override int GetCost()
+    {
+        return 20;
+    }
+}
