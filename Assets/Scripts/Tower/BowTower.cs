214db94f236db96b0df6d5f535ebab664a085ca2
brendon-ai <brendon-m@outlook.com>
2018-01-12 20:39:37 -0500

    lots of things

---
+++ Assets/Scripts/Tower/BowTower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowTower : Tower
+{
+    const float bulletSpeed = 100f;
+
+    [SerializeField] GameObject bullet;
+
+    public override void Shoot(Vector3 position)
+    {
+        var delta = position - transform.position;
+        var force = delta.normalized * bulletSpeed;
+        var bul = Instantiate(bullet);
+        bul.transform.position = transform.position;
+        bul.GetComponent<Rigidbody>().AddForce(force);
+    }
+}
