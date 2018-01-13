1406ffb2cf254f43ef8d5de6a7d940e582e08e3d
Aidan Zerwer <robobug9@gmail.com>
2018-01-13 13:18:44 -0500

    asd

--- Assets/Scripts/Menu/GameOver.cs
+++ Assets/Scripts/Menu/GameOver.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour {
-	
 	// Update is called once per frame
 	void Update () {
-		if (Time.timeSinceLevelLoad >= 5f) {
+		if (Time.timeSinceLevelLoad > 5f) {
 			SceneManager.LoadScene ("Main", LoadSceneMode.Single);
 		}
 	}
