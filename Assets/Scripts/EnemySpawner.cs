e45ff7ce690e35d3db80159b0379cf38404e1f5c
brendon-ai <brendon-m@outlook.com>
2018-01-13 04:19:59 -0500

    Spawners!

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] GameObject enemy;
+    [SerializeField] float spawnsPerSecond;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0f, 1f) < spawnsPerSecond * Time.deltaTime)
+        {
+            Instantiate(enemy);
+        }
+    }
+}
