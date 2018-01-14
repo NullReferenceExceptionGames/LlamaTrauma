d507fb2a1ef769b0c0f5dcb76452a2f041f26bf5
Aidan Zerwer <robobug9@gmail.com>
2018-01-14 10:15:27 -0500

    afgG

---
+++ Assets/Scripts/Menu/ReturnToGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToGame : MonoBehaviour {
+
+	public void back(){
+		var settings = GameObject.FindGameObjectWithTag ("Settings");
+		Destroy (settings);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
