92b95238d3a6e330750f7a479f98bb3dfc85f292
brendon-ai <brendon-m@outlook.com>
2018-02-01 09:13:11 -0500

    Finishing touches for 1.0 release!

--- Assets/Scripts/Menu/SceneChanger.cs
+++ Assets/Scripts/Menu/SceneChanger.cs
@@ -4,31 +4,40 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class SceneChanger : MonoBehaviour {
+public class SceneChanger : MonoBehaviour
+{
 
-	[SerializeField] GameObject sceneModel;
-	[SerializeField] GameObject sceneModel2;
-	[SerializeField] Text StoryText;
-	// Use this for initialization
-	void Start () {
-		Instantiate (sceneModel);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.touches.Length > 0) {
-			SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
-		}
-		if (Time.timeSinceLevelLoad >= 15f) {
-			var scene1 = GameObject.FindGameObjectsWithTag ("Scene");
-			foreach (var i in scene1) {
-				Destroy (i);
-			}
-			Instantiate (sceneModel2);
-			StoryText.text = "They envision a world populated by llamas, farming humans for our hair. We will not submit. We will rid ourselves of this llama scourge and see peace again.";
-		}
-		if (Time.timeSinceLevelLoad >= 30f) {
-			SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
-		}
-	}
+    [SerializeField] GameObject sceneModel;
+    [SerializeField] GameObject sceneModel2;
+    [SerializeField] Text StoryText;
+    bool onSecondDescription = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        Instantiate(sceneModel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touches.Length > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            if (onSecondDescription)
+            {
+                SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+            }
+            else
+            {
+                var scene1 = GameObject.FindGameObjectsWithTag("Scene");
+                foreach (var i in scene1)
+                {
+                    Destroy(i);
+                }
+                Instantiate(sceneModel2);
+                StoryText.text = "They envision a world populated by llamas, farming humans for our hair. We will not submit. We will rid ourselves of this llama scourge and see peace again.";
+                onSecondDescription = true;
+            }
+        }
+    }
 }
